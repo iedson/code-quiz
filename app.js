d450f7a3ad40bf6c75fc4350c6251edc05db6c00
Izzy Edson <isabelle.edson@hyatt.com>
2019-10-13 23:31:53 -0500

    separated landing page from actual quiz, connecting quiz choices with answers

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 /* global variables */
 var questionDiv = document.getElementById('question');
 var choicesDiv = document.getElementById('choices');
-var nextBtn = document.getElementById('nextBtn')
-var questionZero = questions[0].title;
-var timeEl = document.querySelector(".time");
-var secondsLeft = 75;
+var answerDiv = document.getElementById('answer');
+var nextBtn = document.getElementById('nextBtn');
+var choiceBtn = document.getElementById('choiceBtn');
+var scoreDiv = document.getElementById('score');
+var currentQuestion = {};
+var startQuestion = 0;
+var score = 0;
 // array of questions (question~string, choices~array (for loop), answer~string, key~number so you can id in local storage 
 
 // helper function 
@@ -12,21 +15,54 @@ var secondsLeft = 75;
 function renderQuestion(index) {
     return questions[index].title;
 };
-//function renderQuestions
-//for loop of questions in the array in questions.js file 
+//build choices - for loop
+function renderChoices(index){
+    choicesDiv.innerHTML = '';
+    for (i = 0; i < questions[index].choices.length; i++) {
+        var choiceBtn = document.createElement('button');
+        choiceBtn.textContent = questions[index].choices[i];
+        choicesDiv.appendChild(choiceBtn);
+    };
+    choiceBtn.addEventListener('click', function(){
+        choices.innerText = currentQuestion['choice' + answer];
+        if (choice == answer){
+            alert('correct!');
+        } else {
+            alert('Wrong!');
+            // speed up timer by 2 seconds
+        }
+    });
+};
 
-/* events */
-    /* click highscore link */
-        /* find object in local storage */
-        /* parse object for screen */
-        /* display parsed oject in html */
-    /* clear highscores */
-    /* go back */
-
-    // start quiz */
+// events
+    // start quiz 
 nextBtn.addEventListener('click', function(){
+    startQuestion++;
     // call render question 
+    questionDiv.innerHTML=renderQuestion(startQuestion);
+    renderChoices(startQuestion);
 });
+
+// choiceBtn.addEventListener('click', function(){
+//     if(choiceBtn == questions[i].answer){
+//         alert('Correct!');
+//     } else {
+//         alert('WRONG!');
+//         //speed up timer by 2 seconds
+//     };
+// });
+
+
+//score 
+    //show score "You got" + score + "/" + questions.lenght;
+    // enter initials and submit score, go to highscore event 
+    // add to local storage
+    // click highscore link 
+    // find object in local storage 
+    // parse object for screen 
+    // display parsed oject in html
+    // clear highscores 
+
 
 
 // need to get this function talking to the html but only on click of the start button
@@ -42,12 +78,11 @@ nextBtn.addEventListener('click', function(){
 
 //   }, 1000);
 // }
-    /* choose answer */
-    // wrong answer speeds up countdown //
-    /* enter initials and submit score, go to highscore event */
+    // choose answer
+    // wrong answer speeds up countdown
 
 /* init */
-questionDiv.innerHTML = renderQuestion(0);
-choicesDiv.innterHTML = 'replaced by js';
+questionDiv.innerHTML = renderQuestion(startQuestion);
+renderChoices(startQuestion);
 
 
--- questions.js
+++ questions.js
@@ -2,16 +2,16 @@ var questions = [
     {
       title: "Which of the following is a non-essential part of skincare routine?",
       choices: ["cleanser", "moisturizer", "exfoliants", "serums"],
-      answer: "serums"
+      answer: 3
     },
     {
       title: "Which of the following is not an exfoliant",
       choices: ["AHA", "Salicylic Acid", "hyaluronic acid", "retinol"],
-      answer: "hyaluronic acid"
+      answer: 2
     },
     {
-        title: "Each of the following oils are comedongenic and shouldn't be used on your face, ecept for ____",
+        title: "Each of the following oils are comedongenic and shouldn't be used on your face, except for ____",
         choices: ["rosehip seed oil", "shea butter", "avocado oil", "coconut oil"],
-        answer: "rosehip seed oil"
+        answer: 0
     },
   ];
